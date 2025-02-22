25ab09c869040cb43d997a2cf2f713de367f9c3f
Eric Wang <eric4781cool@gmail.com>
2025-02-22 12:11:16 -0600

    map component init

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: false,
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss',
+})
+export class MapComponent implements OnInit, AfterViewInit {
+  private map!: L.Map;
+  markers: L.Marker[] = [
+    L.marker([23.7771, 90.3994]), // Dhaka, Bangladesh
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.initMap();
+    this.centerMap();
+  }
+
+  private initMap() {
+    const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    this.map = L.map('map');
+    L.tileLayer(baseMapURl).addTo(this.map);
+  }
+
+  private centerMap() {
+    // Create a boundary based on the markers
+    const bounds = L.latLngBounds(
+      this.markers.map((marker) => marker.getLatLng())
+    );
+
+    // Fit the map into the boundary
+    this.map.fitBounds(bounds);
+  }
+}
